dffd6ea87795af1cef8899f3569fec2693913b29
Gaurav Nanda <gnanda@TACC-AirSpare02.local>
2014-03-28 22:52:05 -0500

    Updated code to include no-adaptation here.

--- Project3/stanford-parser-full-2014-01-04/DomainAdapterParser.java
+++ Project3/stanford-parser-full-2014-01-04/DomainAdapterParser.java
@@ -62,39 +62,41 @@ class DomainAdapterParser {
 		LexicalizedParser lp = LexicalizedParser.trainFromTreebank(trainTreeBank, null, op);
 		System.out.println("Size of original Tree Bank: " + trainTreeBank.size());
 		
-		// Create memory tree bank for the self-training data.
-		Treebank selfTrainTreebank = op.tlpParams.memoryTreebank();
-		selfTrainTreebank.loadPath(args[1], null);
-		//selfTrainTreebank.loadPath("/Users/gnanda/nlp/repo/Project3/brown/cf/cf01.mrg", null);
-		System.out.println("Size of selfTrain Tree Bank: " + selfTrainTreebank.size());
-		
-		System.out.println("Update tree bank with self trained tree bank...");
-		
-		List<List<CoreLabel>> sentences = new ArrayList<List<CoreLabel>>();
-		for (Tree tree : selfTrainTreebank) {
-			//System.out.println("Old Tree: " + tree.toString());
-			List<CoreLabel> sentence = Sentence.toCoreLabelList(tree.yieldWords());
-			sentences.add(sentence);
-		}	
-		
-		int threads = args.length > 3 ? Integer.parseInt(args[3]) : 1;
-		List<Tree> new_trees = lp.parseMultiple(sentences, threads);
-		
-		for (Tree new_tree : new_trees) {
-			if (new_tree.children()[0].label().toString().equals("X")) {
-				System.out.println("New Tree: " + new_tree.toString());
-				continue;
+		if (!args[1].equals("NOADAPTION")) {
+			// Create memory tree bank for the self-training data.
+			Treebank selfTrainTreebank = op.tlpParams.memoryTreebank();
+			selfTrainTreebank.loadPath(args[1], null);
+			//selfTrainTreebank.loadPath("/Users/gnanda/nlp/repo/Project3/brown/cf/cf01.mrg", null);
+			System.out.println("Size of selfTrain Tree Bank: " + selfTrainTreebank.size());
+			
+			System.out.println("Update tree bank with self trained tree bank...");
+			
+			List<List<CoreLabel>> sentences = new ArrayList<List<CoreLabel>>();
+			for (Tree tree : selfTrainTreebank) {
+				//System.out.println("Old Tree: " + tree.toString());
+				List<CoreLabel> sentence = Sentence.toCoreLabelList(tree.yieldWords());
+				sentences.add(sentence);
+			}	
+			
+			int threads = args.length > 3 ? Integer.parseInt(args[3]) : 1;
+			List<Tree> new_trees = lp.parseMultiple(sentences, threads);
+			
+			for (Tree new_tree : new_trees) {
+				if (new_tree.children()[0].label().toString().equals("X")) {
+					System.out.println("New Tree: " + new_tree.toString());
+					continue;
+				}
+				//System.out.println("New Tree: " + new_tree.toString());
+				trainTreeBank.add(new_tree);
 			}
-			//System.out.println("New Tree: " + new_tree.toString());
-			trainTreeBank.add(new_tree);
+			
+			System.out.println("Done updating tree bank with self trained data..");	
+			System.out.println("Size of updated Tree Bank: " + trainTreeBank.size());
+			
+			System.out.println("Retraining parser..");
+			LexicalizedParser retrained_lp = LexicalizedParser.trainFromTreebank(trainTreeBank, null, op);
+			System.out.println("Retraining done.");
 		}
-		
-		System.out.println("Done updating tree bank with self trained data..");	
-		System.out.println("Size of updated Tree Bank: " + trainTreeBank.size());
-		
-		System.out.println("Retraining parser..");
-		LexicalizedParser retrained_lp = LexicalizedParser.trainFromTreebank(trainTreeBank, null, op);
-		System.out.println("Retraining done.");
 		
 		Treebank testTreeBank = op.tlpParams.memoryTreebank();
 		testTreeBank.loadPath(args[2], null);
